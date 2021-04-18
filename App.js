df71c1767324b0cdeb9b9de5b3a7a090a9c3e31a
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2021-04-17 22:58:24 -0400

    final attempt commit

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import CarItem from './components/CarItem/'
 import CarsList from './components/CarsList';
+import Header from './components/Header';
 //index might work'
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+    <Header />
 
     <CarsList />
 
